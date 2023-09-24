ac3ddcbd208f2c0010193bce74fc583cb4b00d69
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-09-25 00:22:18 +0900

    202020381_W3

--- src/W3/assignment/step3/sketch.js
+++ src/W3/assignment/step3/sketch.js
@@ -1,56 +1,175 @@
-let pos; // 현재 위치 벡터
-let vel; // 현재 속도 벡터
-let acc; // 현재 가속도 벡터
+// 이 코드는 벡터를 배우는 시점에는 이해하려 하지마세요
+
+const sliderMap = new Map();
+
+const bezierEasingGraphSize = 400;
+let bottomLeftX;
+let bottomLeftY;
+let cpAX;
+let cpAY;
+let cpBX;
+let cpBY;
 
 function setup() {
-  createCanvas(400, 400);
-  pos = createVector(width / 2, height / 2);
-  vel = createVector(0, 0);
-  acc = p5.Vector.random2D(); // 초기 가속도를 무작위 방향으로 설정
-  acc.mult(0.02); // 가속도 크기를 조절
+  setCanvasContainer('canvas', 3, 2, true);
+
+  bottomLeftX = width / 2 - bezierEasingGraphSize / 2;
+  bottomLeftY = height / 2 + bezierEasingGraphSize / 2;
+  initGraph();
+
+  background(240);
 }
 
 function draw() {
-  background(220);
-
-  // 마우스 위치에서 원 그리기
-  let target = createVector(mouseX, mouseY);
-  stroke(0);
-  noFill();
-  ellipse(target.x, target.y, 20, 20);
-
-  // 중력 시뮬레이션
-  let gravity = createVector(0, 0.2);
-  acc.add(gravity);
-
-  // 속도에 가속도 더하기
-  vel.add(acc);
-
-  // 위치에 속도 더하기
-  pos.add(vel);
-
-  // 화면 경계 조정
-  if (pos.x > width) {
-    pos.x = 0;
-  } else if (pos.x < 0) {
-    pos.x = width;
-  }
-
-  if (pos.y > height) {
-    pos.y = 0;
-  } else if (pos.y < 0) {
-    pos.y = height;
-  }
-
-  // 속도 벡터 시각화
-  let velVisualization = p5.Vector.add(pos, p5.Vector.mult(vel, 10));
-  line(pos.x, pos.y, velVisualization.x, velVisualization.y);
-
-  // 가속도 벡터 시각화
-  let accVisualization = p5.Vector.add(pos, p5.Vector.mult(acc, 100));
-  line(pos.x, pos.y, accVisualization.x, accVisualization.y);
-
-  // 현재 위치에 원 그리기
-  fill(0);
-  ellipse(pos.x, pos.y, 20, 20);
+  background(240);
+  renderCubicBezierEasingGraph(
+    bottomLeftX,
+    bottomLeftY,
+    bezierEasingGraphSize,
+    cpAX,
+    cpAY,
+    cpBX,
+    cpBY,
+    convertMouseXToT()
+  );
 }
+
+const updateCp = (elem) => {
+  const cpVal = Number(elem.value);
+  sliderMap.get(elem).innerHTML = cpVal.toFixed(6);
+  switch (elem.id) {
+    case 'sliderCpAX':
+      cpAX = cpVal;
+      break;
+    case 'sliderCpAY':
+      cpAY = cpVal;
+      break;
+    case 'sliderCpBX':
+      cpBX = cpVal;
+      break;
+    case 'sliderCpBY':
+      cpBY = cpVal;
+      break;
+  }
+};
+
+const sliderCb = (evt) => {
+  updateCp(evt.target);
+};
+
+const initGraph = () => {
+  document.querySelectorAll('.cpSlider').forEach((eachCpSlider) => {
+    sliderMap.set(
+      eachCpSlider,
+      document.querySelector(`#${eachCpSlider.id.replace('slider', 'val')}`)
+    );
+    eachCpSlider.addEventListener('input', sliderCb);
+    updateCp(eachCpSlider);
+  });
+};
+
+const convertMouseXToT = () => {
+  const constrainedX = constrain(
+    mouseX,
+    bottomLeftX,
+    bottomLeftX + bezierEasingGraphSize
+  );
+  return map(
+    constrainedX,
+    bottomLeftX,
+    bottomLeftX + bezierEasingGraphSize,
+    0,
+    1
+  );
+};
+
+const getCubicBezierEasing = (cpAX, cpAY, cpBX, cpBY, t) => {
+  const bX = bezierPoint(0, cpAX, cpBX, 1, t);
+  const bY = bezierPoint(0, cpAY, cpBY, 1, t);
+  return [bX, bY];
+};
+
+const getScaledNormal = (input, zero, mult) => {
+  return zero + input * mult;
+};
+
+const renderCubicBezierEasingGraph = (
+  bottomLeftX,
+  bottomLeftY,
+  size,
+  cpAX,
+  cpAY,
+  cpBX,
+  cpBY,
+  t
+) => {
+  const endX = getScaledNormal(1, bottomLeftX, size);
+  const endY = getScaledNormal(1, bottomLeftY, -size);
+  const [bX, bY] = getCubicBezierEasing(cpAX, cpAY, cpBX, cpBY, t);
+  const scaledCpAX = getScaledNormal(cpAX, bottomLeftX, size);
+  const scaledCpAY = getScaledNormal(cpAY, bottomLeftY, -size);
+  const scaledCpBX = getScaledNormal(cpBX, bottomLeftX, size);
+  const scaledCpBY = getScaledNormal(cpBY, bottomLeftY, -size);
+  const scaledBX = getScaledNormal(bX, bottomLeftX, size);
+  const scaledBY = getScaledNormal(bY, bottomLeftY, -size);
+
+  rectMode(CENTER);
+  textSize(18);
+
+  // 베지어 곡선
+  noFill();
+  stroke(0, 127, 0);
+  strokeWeight(2);
+  bezier(
+    bottomLeftX,
+    bottomLeftY,
+    scaledCpAX,
+    scaledCpAY,
+    scaledCpBX,
+    scaledCpBY,
+    endX,
+    endY
+  );
+
+  // 베지어 곡선의 조작선
+  stroke(0, 127, 0);
+  strokeWeight(1);
+  line(bottomLeftX, bottomLeftY, scaledCpAX, scaledCpAY);
+  line(scaledCpBX, scaledCpBY, endX, endY);
+
+  // 기준선
+  noFill();
+  stroke(0);
+  line(bottomLeftX, bottomLeftY, endX, bottomLeftY);
+  line(bottomLeftX, bottomLeftY, bottomLeftX, endY);
+
+  // 베지어 곡선의 조작점
+  fill(255);
+  rect(scaledCpAX, scaledCpAY, 6, 6);
+  rect(scaledCpBX, scaledCpBY, 6, 6);
+
+  // 곡선 위 값에 대한 x 보조선
+  noFill();
+  stroke(255, 0, 0);
+  line(scaledBX, bottomLeftY, scaledBX, scaledBY);
+
+  // 곡선 위 값에 대한 y 보조선
+  stroke(0, 0, 255);
+  line(bottomLeftX, scaledBY, scaledBX, scaledBY);
+
+  // 곡선 위 값
+  fill(255);
+  stroke(0);
+  circle(scaledBX, scaledBY, 10);
+
+  // 곡선 위 값의 x값
+  noStroke();
+  fill(255, 0, 0);
+  textAlign(LEFT);
+  text(bX.toFixed(6), scaledBX, bottomLeftY + textAscent());
+
+  // 곡선 위 값의 y값
+  fill(0, 0, 255);
+  textAlign(RIGHT);
+  text(bY.toFixed(6), bottomLeftX - 8, scaledBY);
+};
