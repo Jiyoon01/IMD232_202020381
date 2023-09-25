df0c9a3ce4774e6e1083bb121f87113cc641878b
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-09-25 15:19:59 +0900

    IMD232_9_25

--- src/w4/assignment/step1/script/Mover.js
+++ src/w4/assignment/step1/script/Mover.js
@@ -7,32 +7,34 @@ class Mover {
     this.mass = radius ** (1 / 2);
   }
 
+  applyForce(force) {
+    let divedForce = p5.Vector.div(force, this.mass);
+    this.acc.add(divedForce);
+  }
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
+    this.acc.mult(0);
   }
 
   edgeBounce() {
-    if(this.pos < 0 + this.radius) {
-        let delta = this.pos.x - (0 + this.radius);
-        this.pos.x += -2 * delta;
-        this.vel.x += -1;
-    }   else if(this.pos.x > width -1 this.radius) {
-        let delta = this.pos.x - (width -1 this.radius)
-        this.pos.x += -2 * delta;
-        this.vel.x += -1;
-    }   
-    if(this.pos.y > width -1 this.radius) {
-        let delta = this.pos.x - (height -1 - this.radius)
-        this.pos.y += -2 * delta;
-        this.vel.y += -1;
-    }   else if(this.pos.x > width -1 this.radius) {
-        let delta = this.pos.x - (width -1 this.radius)
-        this.pos.y += -2 * delta;
-        this.vel.y += -1;
-}
-
-display() {
+    if (this.pos < 0 + this.radius) {
+      let delta = this.pos.x - (0 + this.radius);
+      this.pos.x += -2 * delta;
+      this.vel.x += -1;
+    } else if (this.pos.x > width - 1 - this.radius) {
+      let delta = this.pos.x - (width - 1 - this.radius);
+      this.pos.x += -2 * delta;
+      this.vel.x += -1;
+    }
+    if (this.pos.y > width - 1 - this.radius) {
+      let delta = this.pos.x - (height - 1 - this.radius);
+      this.pos.y += -2 * delta;
+      this.vel.y += -1;
+    }
+  }
+  display() {
     ellipse(this.pos.x, this.pos.y, 2 * this.radius);
-}
+  }
 }
--- src/w4/assignment/step1/script/sketch.js
+++ src/w4/assignment/step1/script/sketch.js
@@ -1,11 +1,19 @@
 let ball;
+let gravity;
+let wind;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
   ball = new Mover(width / 2, 0, 50);
+  gravity = createVector(0, 0.1);
+  wind = createVector(-1, -0.01);
 }
 
 function draw() {
+  ball.applyForce(gravity);
+  if (mouseIsPressed) {
+    ball.applyForce(wind);
+  }
   ball.update();
   ball.edgeBounce();
   background('salmon');
