e642d6096de8f87340326c9cdeef7d86b3a90939
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-11-06 04:26:31 +0900

    IMD232 W7 CHOI

--- src/w7/assignment/step1/script/sketch.js
+++ src/w7/assignment/step1/script/sketch.js
@@ -1,25 +1,25 @@
-let traffic; // Traffic 클래스를 담을 변수
-let infiniteOffset = 80; // 물체가 캔버스 밖 공간에서의 허용되는 경계의 길이
+let traffic; // Traffic 객체를 저장할 변수
+let infiniteOffset = 80; // 물체가 캔버스 바깥 공간에서 허용되는 경계 길이
 
 function setup() {
-  // 최초 1회 세팅
-  setCanvasContainer('canvas', 3, 2, true); // index.html의 body에 3:2 비율로 캔버스 생성
-  colorMode(HSL, 360, 100, 100, 100); // HSL 형식으로 색조:360, 채도:100, 명도:100, 범위:100 설정
-  background('white'); // 캔버스 흰색으로 칠하기
+  // 초기 설정
+  createCanvas(windowWidth, windowHeight); // 웹 페이지의 창 크기에 맞게 캔버스 생성
+  colorMode(HSL, 360, 100, 100, 100); // 색상 모드를 HSL로 설정 (색상: 360, 채도: 100, 명도: 100, 알파: 100)
+  background(255); // 캔버스 배경을 흰색으로 설정
   traffic = new Traffic(); // Traffic 객체 생성
   for (let n = 0; n < 10; n++) {
-    // n이 0부터 9까지 1씩 증가하면서
-    traffic.addVehicle(random(width), random(height)); // 캔버스의 랜덤한 위치에 새로운 Vehicle 생성
+    // 0부터 9까지 반복
+    traffic.addVehicle(random(width), random(height)); // 무작위 위치에 새로운 물체 추가
   }
 }
 
 function draw() {
-  // 계속 반복
-  background('white'); // 캔버스 흰색으로 칠하기
-  traffic.run(); // 각각의 객체마다 위치, 속도, 가속도, 힘 설정
+  // 계속해서 실행되는 부분
+  background(255); // 캔버스를 다시 흰색으로 지우기
+  traffic.run(); // 모든 물체들의 움직임과 상호작용 업데이트
 }
 
 function mouseDragged() {
-  // 마우스를 드래깅하는 동안
-  traffic.addVehicle(mouseX, mouseY); // 새로운 객체 생성
+  // 마우스를 드래그하는 동안
+  traffic.addVehicle(mouseX, mouseY); // 새로운 물체를 마우스 위치에 추가
 }
