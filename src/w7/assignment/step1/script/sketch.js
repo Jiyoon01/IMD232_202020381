5a7c909b19feb58ba1c4f2c379ffb23dbc1c6e26
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-11-06 05:11:31 +0900

    IMD232_202020381_W7

--- src/w7/assignment/step1/script/sketch.js
+++ src/w7/assignment/step1/script/sketch.js
@@ -3,7 +3,7 @@ let infiniteOffset = 80; // 물체가 캔버스 바깥 공간에서 허용되는
 
 function setup() {
   // 초기 설정
-  createCanvas(windowWidth, windowHeight); // 웹 페이지의 창 크기에 맞게 캔버스 생성
+  setCanvasContainer('canvas', 3, 2, true); // 3:2 비율로 캔버스 생성
   colorMode(HSL, 360, 100, 100, 100); // 색상 모드를 HSL로 설정 (색상: 360, 채도: 130, 명도: 100, 알파: 80)
   background(255); // 캔버스 배경을 흰색으로 설정
   traffic = new Traffic(); // Traffic 객체 생성
