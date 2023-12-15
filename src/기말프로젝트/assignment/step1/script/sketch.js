599d0fee56156c8448afdaeba396816e5f6ade5c
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-12-15 13:56:04 +0900

    IMD232_202020381_FINAL

--- src/기말프로젝트/assignment/step1/script/sketch.js
+++ src/기말프로젝트/assignment/step1/script/sketch.js
@@ -8,7 +8,7 @@ let wave = {
   amplitude: 50,
   frequency: 0.02,
   phase: 0,
-  speed: 0.1,
+  speed: 0.05,
 };
 
 function setup() {
@@ -18,11 +18,16 @@ function setup() {
 function draw() {
   background(255);
 
+  noFill();
+  strokeWeight(2);
   beginShape();
+
   for (let x = 0; x < width; x += 10) {
     const y = wave.amplitude * sin(wave.frequency * x + wave.phase);
-    vertex(x, y + height / 2);
+    const distortion = map(noise(x * 0.01, frameCount * 0.005), 0, 1, -10, 10);
+    curveVertex(x + distortion, y + height / 2 + distortion);
   }
+
   endShape();
 
   wave.phase += wave.speed;
