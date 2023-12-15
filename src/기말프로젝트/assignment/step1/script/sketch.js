e1aafdf3d5c012212eb4ba1fdcaf93b67e378b38
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-12-16 00:58:19 +0900

    IMD232_202020381_FINAL

--- src/기말프로젝트/assignment/step1/script/sketch.js
+++ src/기말프로젝트/assignment/step1/script/sketch.js
@@ -4,72 +4,69 @@ function setup() {
   background(255);
 }
 
-// 이 클래스는 각 파티클(물방울)의 속성들을 표현합니다.
-class Particle {
-  // 파티클의 좌표값, 반경, 속도, 그리고 생명주기 설정
-  constructor() {
-    this.x = mouseX;
-    this.y = mouseY;
-    this.radius = random(10, 20);
-    this.xSpeed = random(-2, 2);
-    this.ySpeed = random(-2, 2);
-    this.life = 255; // 초기 생명주기
-  }
+let fire;
+let flameSound;
 
-  // 파티클 그리기
-  drawParticle() {
-    noStroke();
-    fill(0, 0, 255, this.life);
-    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
-  }
-
-  // 파티클 이동
-  moveParticle() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
-  }
-
-  // 파티클 생명주기 감소
-  decreaseLife() {
-    this.life -= 2;
-  }
-
-  // 파티클이 소멸되었는지 확인
-  isDead() {
-    return this.life <= 0;
-  }
+function preload() {
+  flameSound = loadSound('flame.mp3'); // 적절한 화염 소리 파일을 사용하세요
 }
 
-// 복수의 파티클들을 추가하기 위한 배열
-let particles = [];
-
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(800, 500);
+  fire = new Fire(width / 2, height - 50);
+  frameRate(30);
 }
 
 function draw() {
-  background(255);
+  background(0);
 
-  // 모든 파티클 그리기 및 이동
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].drawParticle();
-    particles[i].moveParticle();
-    particles[i].decreaseLife();
+  fire.update();
+  fire.display();
+}
 
-    // 파티클이 소멸되었을 경우 배열에서 제거
-    if (particles[i].isDead()) {
-      particles.splice(i, 1);
-      i--;
-    }
+function mousePressed() {
+  if (!flameSound.isPlaying()) {
+    flameSound.play();
   }
 }
 
-// 마우스를 클릭할 때마다 새로운 파티클(물방울) 생성
-function mouseClicked() {
-  particles.push(new Particle());
-}
+class Fire {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 120;
+    this.intensity = 1.5;
+  }
 
-// 마우스를 드래그하는 동안 파티클 계속 생성
-function mouseDragged() {
-  particles.push(new Particle());
+  update() {
+    this.size = noise(frameCount * 0.02) * 150 + 50;
+    this.intensity = noise(frameCount * 0.05) * 3 + 1;
+  }
+
+  display() {
+    let fireColor = color(255, 150, 0, 200);
+
+    for (let i = 0; i < 20; i++) {
+      let offsetX = random(-50, 50);
+      let offsetY = random(-50, 50);
+
+      let d = dist(this.x, this.y, mouseX + offsetX, mouseY + offsetY);
+
+      let alpha = map(d, 0, 200, 255, 0);
+      alpha = constrain(alpha, 0, 255);
+
+      fill(
+        fireColor.levels[0],
+        fireColor.levels[1],
+        fireColor.levels[2],
+        alpha
+      );
+
+      let size = this.size + random(-20, 20);
+
+      // 장작을 피웠을 때 타오르는 불의 형태로 변경
+      let flameShape = sin((frameCount + i) * 0.1) * 20; // Sin 곡선 모양
+      ellipse(this.x + offsetX, this.y + offsetY + flameShape, size, size);
+    }
+  }
 }
