adbadf9af76809c316daf4cc97704d9010ff5825
Jiyoon <143991709+Jiyoon01@users.noreply.github.com>
2023-12-16 03:53:33 +0900

    IMD232_202020381_FINAL

--- src/기말프로젝트/assignment/step1/script/sketch.js
+++ src/기말프로젝트/assignment/step1/script/sketch.js
@@ -1,37 +1,59 @@
-function setup() {
-  setCanvasContainer('canvas', 3, 2, true);
-  colorMode(HSL, 360, 100, 100, 100);
-  background(255);
-}
-
-let mic;
-let fft;
+let bolts = [];
 
 function setup() {
-  createCanvas(800, 400);
-
-  // 마이크 입력 설정
-  mic = new p5.AudioIn();
-  mic.start();
-
-  // FFT(Fast Fourier Transform) 객체 생성
-  fft = new p5.FFT();
-  fft.setInput(mic);
+  createCanvas(800, 600);
+  strokeWeight(4); // 선분의 두께를 8로 설정
 }
 
 function draw() {
-  background(0);
+  background(0); // 검은 배경
 
-  // 주파수 스펙트럼 분석
-  let spectrum = fft.analyze();
+  for (let i = bolts.length - 1; i >= 0; i--) {
+    bolts[i].update();
+    bolts[i].show();
+    if (bolts[i].isFinished()) {
+      bolts.splice(i, 1);
+    }
+  }
+}
 
-  noStroke();
-  fill(255);
+function mouseMoved() {
+  let bolt = new Bolt(mouseX, mouseY);
+  bolts.push(bolt);
+}
 
-  // 주파수 스펙트럼을 바 형태로 그림
-  for (let i = 0; i < spectrum.length; i++) {
-    let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h);
+class Bolt {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.length = random(60, 90);
+    this.angle = random(TWO_PI);
+    this.branches = 30; // 번개의 갈래 수를 2로 설정
+    this.branchLengthFactor = 0.75; // 갈래의 길이 비율
+    this.life = 200; // 번개가 화면에 유지되는 시간 (조절 가능)
+  }
+
+  update() {
+    this.life--;
+  }
+
+  show() {
+    stroke(255, 240, 0, this.life);
+    for (let i = 0; i < this.branches; i++) {
+      let endX = this.x + cos(this.angle) * this.length;
+      let endY = this.y + sin(this.angle) * this.length;
+
+      line(this.x, this.y, endX, endY);
+
+      this.x = endX;
+      this.y = endY;
+
+      this.angle += random(-PI / 3, PI / 3);
+      this.length *= this.branchLengthFactor;
+    }
+  }
+
+  isFinished() {
+    return this.life <= 0;
   }
 }
